5d97e273e86fa52d369a888c0041138b7aba3491
Sailakshmi <142158089+SaiLakshmi-github@users.noreply.github.com>
2023-10-01 18:46:19 +0530

    changes made to the script

--- Shellscripting/variable.sh
+++ Shellscripting/variable.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 Date=$(date)
 Uptime=$(uptime)
-echo "Hi, This script is executed in the timestamp:$Date and uptime of VM:$Uptime"
+echo "Hi, This script is executed in the timestamp:$Date"
+echo "Uptime:$Uptime"
