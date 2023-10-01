d2adf3744f930fe5930876963212bea7945829bf
Sailakshmi <142158089+SaiLakshmi-github@users.noreply.github.com>
2023-10-01 18:50:06 +0530

    changes made to the script

---
+++ Shellscripting/01-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+Person1=$1
+Person2=$2
+echo "Hello World! I am learning shell scripting.."
+echo "$Person1: Hi $Person2, Good Morning"
+echo "$Person2: Good morning, $Person1"
+echo "$Person1: How are you, $Person2"
+echo "$Person2:I am good, $Person1"
