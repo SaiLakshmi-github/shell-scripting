d4611608f9860cc1803883cffd48e5c01871f854
Sailakshmi <142158089+SaiLakshmi-github@users.noreply.github.com>
2023-10-01 18:24:24 +0530

    changes made to the script

--- Shellscripting/shellscript.sh
+++ Shellscripting/shellscript.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 echo "Hello World! I am learning shell scripting.."
+echo "Ramesh: Hi Suresh, Good Morning"
+echo "Suresh: Good morning, Ramesh"
+echo "Ramesh: How are you, Suresh"
+echo "Suresh:I am good, Ramesh"
