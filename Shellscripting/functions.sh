967860e0f021ebfea7bd4531c90321eb2948158d
Sailakshmi <142158089+SaiLakshmi-github@users.noreply.github.com>
2023-10-16 23:50:16 +0530

    installing sql

--- Shellscripting/functions.sh
+++ Shellscripting/functions.sh
@@ -2,3 +2,24 @@
 Date=$(date +%F-%H-%M-%S)
 Script_name=$0
 Logfile=/tmp/$Script_name-$Date.log
+
+Validate(){
+    if [$1 -ne 0]
+    then
+    echo "$2...Failure"
+    else
+    echo "$2..Success"
+fi
+}
+UserID=$(id -u)
+if [$UserID -ne 0]
+then
+echo "Error:: please run this script with root access"
+exit 1
+fi
+yum install mysql -y &>>$Logfile
+Validate $? "installing mysql"
+yum install postfix -y &>>$Logfile
+Validate $? "installing Postfix"
+
+
